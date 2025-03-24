18b7696658df35791fc8dbe370a8b4a30d5e3a58
FavourAmanze <amanzefavour@gmail.com>
2025-03-24 18:47:47 +0000

    todo 2-4 need t do 4 code comments

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -29,8 +29,9 @@ var background = function (window) {
         // ANIMATION VARIABLES HERE //////////////////////////////////////
         //////////////////////////////////////////////////////////////////
         // TODO (several):
-      
-      
+        var tree;
+        var buildings = [];
+
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
@@ -43,18 +44,36 @@ var background = function (window) {
             background.addChild(backgroundFill); // adds the background rectangle to the bacground container so it is visible
             
             // TODO 2: - Add a moon and starfield
+            for (var i = 0; i < 100; i++){
+                var circle = draw.circle(0.5, "white", "white", 2); // create a circle with a specified radius, border color, fill color, alpha and store it in the variable circle
+                circle.x = canvasWidth * Math.random(); // set random x position within canvas width
+                circle.y = groundY * Math.random(); // set random y position within groundY range
+                background.addChild(circle); // adds the star to the background container
+            }
+            
             var moon = draw.bitmap("img/moon.png"); // creates a bitmap object using the moon image and stores it in the moon variable
             moon.x = canvasWidth - 400; //sets the moon's x position 
             moon.y = groundY - 400; // sets the moon's y position
             moon.scaleX = .25; //scales the moon's width
             moon.scaleY = .25; //scales the moon's height
             background.addChild(moon); // add the moon to the background container so it's visible
-            
+        
             // TODO 4: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
+            for (var i = 0; i < 5; i++) {
+                var buildingColors = ["red", "purple", "yellow", "orange", "pink"]
+                var buildingHeight = 300 * Math.random (); // assign 300 to the building height variable
+                var building = draw.rect(75, buildingHeight, buildingColors[i], "Black", 1); // draws a rect with 75 width, buildingHeight is the height, lightgrey is the fill color, black is the outline, and 1 is the outline width
+                building.x = 200 * i; // multiply 200 by the current i value and stores it as the x pos for the building
+                building.y = groundY - buildingHeight; // takes the groundY, subtracts the buildingHeight and stores it as the y value
+                background.addChild(building); // add our bulding to the background container
+                buildings.push(building); // add the building to the buildings array for further manipulation
+              }
             
             // TODO 3: Part 1 - Add a tree
-            
+            tree = draw.bitmap("img/tree.png"); // creates a bitmap for the tree image and stores it in the variable tree
+            tree.x = canvasWidth; // place the tree off screen to the right
+            tree.y = groundY - 230; // place the tree above ground (adjusted for tree height)
+            background.addChild(tree); // adds the tree to the background container
             
         } // end of render function - DO NOT DELETE
         
@@ -68,10 +87,20 @@ var background = function (window) {
             var groundY = ground.y;
             
             // TODO 3: Part 2 - Move the tree!
-            
+            tree.x -= 3; // moves the tree to the left by subtracting 3 from its current x pos
+            if (tree.x < -200){
+                tree.x = canvasWidth;
+            } // checks if the tree goes off the left side of the canvas and if it does it reset the x pos of the tree to the right side of the canvas
             
             // TODO 4: Part 2 - Parallax
-            
+            for (var i = 0; i < buildings.length; i++) {
+                var building = buildings[i]; 
+                building.x -= 1;
+                if (building.x < -200){
+                    building.x = canvasWidth;
+                }
+                // 
+              }
 
         } // end of update function - DO NOT DELETE
         
